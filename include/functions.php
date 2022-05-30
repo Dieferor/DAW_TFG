bb278352571aac802a29006e3a3d92c8b057ea76
Diego Fdez Ortega <diego.ferort.1@educa.jcyl.es>
2022-05-30 21:38:38 +0200

    com2

--- include/functions.php
+++ include/functions.php
@@ -1,6 +1,7 @@
 <?php
 
-$db = mysqli_connect("localhost","root","","carval_db");
+// $db = mysqli_connect("localhost","root","","carval_db");
+$db = mysqli_connect("eu-cdbr-west-02.cleardb.net","b35268faebf5d7","f04c963b","heroku_a2d6bc5845573a1");
 
 function getFeaturedProducts()
 {
