fd12e1a2cd97edebb2b572a707d6ab8eb08c9105
Diego Fdez Ortega <diego.ferort.1@educa.jcyl.es>
2022-05-30 23:06:02 +0200

    c1

--- include/db.php
+++ include/db.php
@@ -1,6 +1,7 @@
 <?php
 
-$db = mysqli_connect("localhost","root","","carval_db");
+// $db = mysqli_connect("localhost","root","","carval_db");
+$db = mysqli_connect("eu-cdbr-west-02.cleardb.net","bb931ab51ffd16","b03cd418","heroku_9b80f2ce09a0ab3");
 
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
